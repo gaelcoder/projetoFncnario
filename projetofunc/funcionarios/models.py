f70b5a822c2f8bc7aa9458e8f36f5f6a544e1bc3
Gabriel <minifinikogama@gmail.com>
2024-08-31 01:16:27 -0300

    Conexão com banco de dados realizada e criação da classe Funcionário feita com sucesso

--- projetofunc/funcionarios/models.py
+++ projetofunc/funcionarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Funcionario(models.Model):
+    apelido = models.CharField(max_length=32)
+    nome_Completo = models.CharField(max_length=100)
+    data_Nasc = models.DateField()
+    stacks = models.TextField(blank=True)
+
+
